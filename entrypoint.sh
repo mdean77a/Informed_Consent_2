53706f59b10279cb38daf4b9854a9af45f8c4e09
Mike Dean <miketraindoc@gmail.com>
2025-01-06 15:46:18 -0700

    Try to install uv from script

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting Qdrant server..."
 /qdrant/qdrant  &
 
 echo "Qdrant is ready!"
-
+curl -LsSf https://astral.sh/uv/install.sh | sh
 # Start Chainlit application using Uvicorn
 echo "Starting Chainlit application..."
 echo "Current path: $PATH"
