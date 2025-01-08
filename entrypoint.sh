778e9cde975da01ac9bffda954e05de886c231e6
Mike Dean <miketraindoc@gmail.com>
2025-01-08 11:15:38 -0700

    Added some echo statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 echo "Current directory: $(pwd)"
 echo "Current path: $PATH"
+
+directory="../root/.local/bin"
+if [ -d "$directory" ];
+then
+    echo "Directory exists: $directory"
+    echo "Files in $directory:"
+    find "$directory" -type f
+else
+    echo "Directory does not exist: $directory"
+fi
 # Start Qdrant in the background
 echo "Starting Qdrant server..."
 /qdrant/qdrant  &
